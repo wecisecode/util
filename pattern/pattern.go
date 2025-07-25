4fada4371181a7b23065d0f3254ebc89422e76de
wecisecode <13611359572@139.com>
2025-07-25 10:54:21 +0800

    Update pattern.go

--- pattern/pattern.go
+++ pattern/pattern.go
@@ -1,16 +1,16 @@
 package pattern
 
-func Wildcard2RegexpString(wildcard string) string {
+func wildcard2RegexpString(wildcard string, regular bool) string {
 	pattern := []rune(wildcard)
 	prunes := []rune{}
 	for i := 0; i < len(pattern); i++ {
 		c := pattern[i]
 		if c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z' || c >= '0' && c <= '9' || c == '_' || c > 0x00FF {
-			if i == 0 {
+			if i == 0 && regular {
 				prunes = append(prunes, '^')
 			}
 			prunes = append(prunes, c)
-			if i == len(pattern)-1 {
+			if i == len(pattern)-1 && regular {
 				prunes = append(prunes, '$')
 			}
 		} else if c == '*' {
@@ -24,14 +24,27 @@ func Wildcard2RegexpString(wildcard string) string {
 			prunes = append(prunes, '\\', c)
 		}
 	}
-	return "(?s)" + string(prunes)
+	if regular {
+		return "(?s)" + string(prunes)
+	}
+	return string(prunes)
 }
 
-func PathWildcard2RegexpString(wildcardpath string) string {
+func Wildcard2RegexpString(wildcard string) string {
+	return wildcard2RegexpString(wildcard, true)
+}
+
+func Wildcard2SimpleRegexpString(wildcard string) string {
+	return wildcard2RegexpString(wildcard, false)
+}
+
+func pathWildcard2RegexpString(wildcardpath string, regular bool) string {
 	pattern := []rune(wildcardpath)
 	prunes := []rune{}
 	if len(wildcardpath) > 0 && wildcardpath[0] == '/' || len(wildcardpath) > 2 && wildcardpath[1:3] == `:\` {
-		prunes = append(prunes, '^')
+		if regular {
+			prunes = append(prunes, '^')
+		}
 	}
 	for i := 0; i < len(pattern); i++ {
 		c := pattern[i]
@@ -54,35 +67,39 @@ func PathWildcard2RegexpString(wildcardpath string) string {
 			prunes = append(prunes, '\\', c)
 		}
 	}
-	return "(?s)" + string(prunes)
+	if regular {
+		return "(?s)" + string(prunes)
+	}
+	return string(prunes)
+}
+
+func PathWildcard2RegexpString(wildcardpath string) string {
+	return pathWildcard2RegexpString(wildcardpath, true)
+}
+
+func PathWildcard2SimpleRegexpString(wildcardpath string) string {
+	return pathWildcard2RegexpString(wildcardpath, false)
+}
+
+func text2RegexpString(text string) string {
+	pattern := []rune(text)
+	prunes := []rune{}
+	for i := 0; i < len(pattern); i++ {
+		c := pattern[i]
+		if c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z' || c >= '0' && c <= '9' || c == '_' || c > 0x00FF {
+			prunes = append(prunes, c)
+		} else {
+			// 转义特殊字符
+			prunes = append(prunes, '\\', c)
+		}
+	}
+	return string(prunes)
 }
 
 func Equal2RegexpString(equal string) string {
-	pattern := []rune(equal)
-	prunes := []rune{}
-	for i := 0; i < len(pattern); i++ {
-		c := pattern[i]
-		if c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z' || c >= '0' && c <= '9' || c == '_' || c > 0x00FF {
-			prunes = append(prunes, c)
-		} else {
-			// 转义特殊字符
-			prunes = append(prunes, '\\', c)
-		}
-	}
-	return "(?s)^" + string(prunes) + "$"
+	return "(?s)^" + text2RegexpString(equal) + "$"
 }
 
 func Contain2RegexpString(contain string) string {
-	pattern := []rune(contain)
-	prunes := []rune{}
-	for i := 0; i < len(pattern); i++ {
-		c := pattern[i]
-		if c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z' || c >= '0' && c <= '9' || c == '_' || c > 0x00FF {
-			prunes = append(prunes, c)
-		} else {
-			// 转义特殊字符
-			prunes = append(prunes, '\\', c)
-		}
-	}
-	return "(?s)" + string(prunes) + ""
+	return "(?s)" + text2RegexpString(contain) + ""
 }
